1d0bc4a7384faa67206f37e84d31a1d25692cb52
Manuel Marugan <manuel.marugan@fon.com>
2018-09-19 16:20:52 +0200

    Return to kubernetes xenial repo

--- provision/shell/install_docker.sh
+++ provision/shell/install_docker.sh
@@ -3,7 +3,7 @@
 #It avoids dpkg-reconfigure using stdin
 export DEBIAN_FRONTEND=noninteractive
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 2>&1 >/dev/null
 
 add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/debian/
@@ -11,6 +11,5 @@ $(lsb_release -cs)
 stable"
 
 apt-get update &&
-#declare -r DOCKER_VERSION_CERTIFIED="17.03.2"
-#apt-get install -qy docker-ce="${DOCKER_VERSION_CERTIFIED}~ce-0~debian-stretch"
-apt-get install -qy docker-ce
+declare -r DOCKER_VERSION_CERTIFIED="17.03.3"
+apt-get install -qy docker-ce="${DOCKER_VERSION_CERTIFIED}~ce-0~debian-stretch"
--- provision/shell/install_kubetools.sh
+++ provision/shell/install_kubetools.sh
@@ -3,9 +3,9 @@
 #It avoids dpkg-reconfigure using stdin
 export DEBIAN_FRONTEND=noninteractive
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 2>&1 >/dev/null
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-stretch main
+deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
 apt-get update &&
